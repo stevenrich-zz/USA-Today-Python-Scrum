7353658f6af27cdf57872a54e8bca19b5c9180e8
Steven Rich <steve@Stevens-MacBook-Air.local>
2012-06-25 12:36:15 -0400

    99 Bottles of Beer on the Wall
    Added a python iteration of the song.

---
+++ beer.py
@@ -0,0 +1,13 @@
+x = 99
+beer = " bottles of beer on the wall"
+beer2 = " bottles of beer"
+take = "take one down"
+passs = "pass it around"
+while x > 0:
+    print str(x) + beer
+    print str(x) + beer2
+    print take
+    print passs
+    print str(x) + beer
+    x = x - 1
+print "You are out of beer. :("
